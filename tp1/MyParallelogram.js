b862e62b310c1d07e45bf25f9197e4ea6b86337f
up202108832 <up202108832@up.pt>
2024-02-16 17:51:56 +0000

    small change

--- tp1/MyParallelogram.js
+++ tp1/MyParallelogram.js
@@ -11,26 +11,20 @@ export class MyParallelogram extends CGFobject {
 	}
 	
 	initBuffers() {
-		this.vertices = [
-			0, 0, 0,	//0
-			1, 0, 0,	//1
-			1, 1, 0,	//2
-            2, 0, 0,    //3
-            2, 1, 0,    //4
-            3, 1, 0,    //5
-		];
+        this.vertices = [
+            0, 0, 0,    //0
+            1, 1, 0,    //1
+            2, 0, 0,    //2
+            3, 1, 0,    //3
+        ];
 
-		//Counter-clockwise reference of vertices
-		this.indices = [
-			0, 1, 2,
-            2, 1, 0,
-            1, 3, 4,
-            4, 3, 1,
-            1, 4, 2,
-            2, 4, 1,
-            3, 5, 4,
-            4, 5, 3,
-		];
+        //Counter-clockwise reference of vertices
+        this.indices = [
+            0, 2, 1,
+            2, 3, 1,
+            1, 2, 0,
+            1, 3, 2
+        ];
 
 		//The defined indices (and corresponding vertices)
 		//will be read in groups of three to draw triangles
