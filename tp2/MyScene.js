62698c13d484465fbf3502502c9f0a5cb15ab89a
Luís Du <up202105385@fe.up.pt>
2024-02-23 13:42:15 +0000

    tangram done

--- tp2/MyScene.js
+++ tp2/MyScene.js
@@ -48,9 +48,9 @@ export class MyScene extends CGFscene {
     );
   }
   setDefaultAppearance() {
-    this.setAmbient(0.2, 0.4, 0.8, 1.0);
-    this.setDiffuse(0.2, 0.4, 0.8, 1.0);
-    this.setSpecular(0.2, 0.4, 0.8, 1.0);
+    this.setAmbient(0.0, 1.0, 0.0, 1.0);
+    this.setDiffuse(0.0, 1.0, 0.0, 1.0);
+    this.setSpecular(0.0, 0.0, 0.0, 1.0);
     this.setShininess(10.0);
   }
   display() {
