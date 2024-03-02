11cf6365d808221cea2baac3e8d548948023f5fb
DanielCarneiro123 <danielmc2116@gmail.com>
2024-03-02 17:53:26 +0000

    first exercise DONE

--- tp3/MyUnitCube.js
+++ tp3/MyUnitCube.js
@@ -67,6 +67,33 @@ export class MyUnitCube extends CGFobject {
             0, 4, 7
 		];
 
+            this.normals = [
+            0, 0, -1,
+            0, 0, -1,	
+            0, 0, -1,
+            0, 0, -1,
+            0, 0, 1,
+            0, 0, 1,
+            0, 0, 1,     
+            0, 0, 1,
+            0, -1, 0,
+            0, 1, 0,
+            0, 1, 0,
+            0, -1, 0,
+            0, -1, 0,
+            0, 1, 0,
+            0, 1, 0,
+            0, -1, 0,
+            1, 0, 0,
+            1, 0, 0,
+            -1, 0, 0,
+            -1, 0, 0,
+            1, 0, 0,
+            1, 0, 0,
+            -1, 0, 0,
+            -1, 0, 0,
+            ];
+
 		//The defined indices (and corresponding vertices)
 		//will be read in groups of three to draw triangles
 		this.primitiveType = this.scene.gl.TRIANGLES;
