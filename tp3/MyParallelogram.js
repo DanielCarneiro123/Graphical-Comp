a2f370ceee68ca50a9ce916322890254526e0847
DanielCarneiro123 <danielmc2116@gmail.com>
2024-03-06 14:03:15 +0000

    added forgotten normal vectors

--- tp3/MyParallelogram.js
+++ tp3/MyParallelogram.js
@@ -16,15 +16,30 @@ export class MyParallelogram extends CGFobject {
             1, 1, 0,    //1
             2, 0, 0,    //2
             3, 1, 0,    //3
+            0, 0, 0,    //4
+            1, 1, 0,    //5
+            2, 0, 0,    //6
+            3, 1, 0,    //7
         ];
 
         //Counter-clockwise reference of vertices
         this.indices = [
             0, 2, 1,
             2, 3, 1,
-            1, 2, 0,
-            1, 3, 2
+            7, 6, 4,
+            5, 7, 4
         ];
+
+        this.normals = [
+            0, 0, 1,
+			0, 0, 1,
+			0, 0, 1,
+			0, 0, 1,
+			0, 0, -1,
+			0, 0, -1,
+			0, 0, -1,
+			0, 0, -1		
+		]
 
 		//The defined indices (and corresponding vertices)
 		//will be read in groups of three to draw triangles
