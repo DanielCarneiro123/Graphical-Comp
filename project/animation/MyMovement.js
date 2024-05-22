8e3998c869078bd544fdfa83965cf2e10c210d19
DanielCarneiro123 <danielmc2116@gmail.com>
2024-05-22 16:34:58 +0100

    update

--- project/animation/MyMovement.js
+++ project/animation/MyMovement.js
@@ -1,14 +1,56 @@
-import { MyAnimation } from "./MyAnimation.js";
-
-export class MyMovement extends MyAnimation {
+export class MyMovement {
   constructor(startVal, endVal, animDurationSecs, loop, active) {
-    super(startVal, endVal, animDurationSecs, loop, active);
     this.x = 0;
     this.y = 0;
     this.z = 0;
     this.wingAngle = 0;
     this.grassAngle = 0;
+    this.startVal = startVal;
+    this.endVal = endVal;
+    this.animDurationSecs = animDurationSecs;
+    this.length = (this.endVal - this.startVal);
+    this.animVal = this.startVal;
+    this.active = active;
+    this.loop = loop;
+    this.last = 0;
   }
+
+  enable(timeSinceAppStart) {
+    this.active = true;
+    this.last = timeSinceAppStart
+    this.lastIteration = false;
+}
+
+disable() {
+    this.active = false;
+}
+
+update(elapsedTimeSecs, vector, movingY) {
+    
+    if (this.active) {
+        let timeSinceAnimationStart = elapsedTimeSecs - this.last;
+        if (this.loop || (timeSinceAnimationStart >= 0 && !this.lastIteration)) {
+            if(!(timeSinceAnimationStart <= this.animDurationSecs) && !this.loop){
+                if (movingY) {
+                    this.animVal = this.movementFunction(1) * this.length;
+                }
+                else {
+                    this.animVal = this.startVal + this.movementFunction(1) * this.length;
+                }
+                this.updatePositionObj(timeSinceAnimationStart, vector, movingY);
+                this.lastIteration = true
+            }else{
+                if (movingY) {
+                    this.animVal = this.movementFunction(timeSinceAnimationStart / this.animDurationSecs) * this.length;
+                }
+                else {
+                    this.animVal = this.startVal + this.movementFunction(timeSinceAnimationStart / this.animDurationSecs) * this.length;
+                }
+                this.updatePositionObj(timeSinceAnimationStart, vector, movingY);
+            }
+        }
+    }
+}
 
   updatePositionObj(elapsedTime, vector, movingY) {
     const objSpeed = this.calculateStaticSpeed(vector.speed);
